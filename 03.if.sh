d2fb1f7369e4171a0bc9181785fbf7010bcf7d59
vijayabhaskar <vijayabhaskarmakke@gmail.com>
2023-01-04 16:45:18 +0530

    if condition

--- 03.if.sh
+++ 03.if.sh
@@ -6,3 +6,15 @@ then
   else
     echo $a is less than 10
   fi
+
+x="abc"
+if [ $x == "abc" ]; then
+  echo Yes both are equal
+  fi
+
+file=/tmp/new
+if [ -f $file ]; then
+  echo file exits
+  else
+    echo file does not exist
+  fi
